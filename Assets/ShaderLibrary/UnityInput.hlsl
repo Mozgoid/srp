0f39f3ab417f57c7964200e3aae6d408df454993
Sasha Shukalo <mozgoid@gmail.com>
2022-01-29 15:51:41 +0300

    furst shader

---
+++ Assets/ShaderLibrary/UnityInput.hlsl
@@ -0,0 +1,18 @@
+#ifndef CUSTOM_UNITY_INPUT_INCLUDED
+#define CUSTOM_UNITY_INPUT_INCLUDED
+
+float4x4 unity_ObjectToWorld;
+float4x4 unity_WorldToObject;
+
+float4x4 unity_MatrixVP;
+float4x4 unity_MatrixV;
+float4x4 glstate_matrix_projection;
+
+#define real float
+#define real3 float3
+#define real3x3 float3x3
+#define real4 float4
+real4 unity_WorldTransformParams;
+
+
+#endif
